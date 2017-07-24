0449308993b6bda76e09f03c25a03451a831593e
quanweilena <quanweilena@gmail.com>
2017-07-23 20:58:48 -0400

    add registration MongoDB model

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ mongoose.connect(config.uri, (err) => {
 });
 
 // Provide static directory for frontend
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/client/dist/'));
 app.use('/authentication', authentication);
 
--- routes/authentication.js
+++ routes/authentication.js
@@ -5,8 +5,48 @@ module.exports = (router) => {
 	router.post('/register', (req, res) => {
 		if (!req.body.email) {
 			res.json({ success: false, message: 'You must provide an E-mail'});
+		} else {
+			if (!req.body.username) {
+				res.json({ success: false, message: 'You must provide a username'});
+			} else {
+				if (!req.body.password) {
+					res.json({ success: false, message: 'You must provide a password'});
+				} else {
+					let user = new User({
+						email: req.body.email.toLowerCase(),
+						username: req.body.username.toLowerCase(),
+						password: req.body.password
+					});
+					user.save((err) => {
+						if (err) {
+							if (err.code === 11000) {
+								res.json({ success: false, message: 'Username or E-mail already exists.'});
+							} else {
+								if (err.errors) {
+									if (err.errors.email) {
+										res.json({ success:false, message: err.errors.email.message});
+									} else {
+										if (err.errors.username) {
+											res.json({ success:false, message: err.errors.username.message});
+										} else {
+											if (err.errors.password) {
+												res.json({ success:false, message: err.errors.password.message});
+											} else {
+												res.json({ success:false, message: err});
+											}
+										}
+									}
+								} else {
+									res.json({ success: false, message: 'Could not save user. Error: ', err});
+								}
+							}
+						} else {
+							res.json({ success: true, message: 'Account registered!'});
+						}
+					});
+				} 
+			} 
 		};
-		res.send('Hello world');
 	});
 
 	return router;
