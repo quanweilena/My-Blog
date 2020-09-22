b3beda0a5eb583a6ddcd78442cc1cd2c1cca9430
Dan Zhou <quanweilena@gmail.com>
2020-09-22 16:14:37 -0400

    updated mongodb conn str

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const crypto = require('crypto').randomBytes(256). toString('hex');
 
 module.exports = {
 	// uri: 'mongodb://localhost:27017/mean-angular-2',
-	uri: 'mongodb://quanweilena:19911221@ds129003.mlab.com:29003/myblog-danzhou',
+	uri: 'mongodb+srv://quanweilena:19911221@myblog-danzhou.8zlqb.mongodb.net/myblog-danzhou?retryWrites=true&w=majority',
 	secret: crypto,
 	db: 'myblog-danzhou'
 }
